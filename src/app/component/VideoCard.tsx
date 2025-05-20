b4e27de70ed8102f98ed8a4793c382d82567bf77
Aashutosh Kumar Bhardwaj <ashutoshkumarbhardwaj7@gmail.com>
2025-05-20 21:53:37 +0530

    new one

---
+++ src/app/component/VideoCard.tsx
@@ -0,0 +1,25 @@
+export function Videocard(){
+    return <div>
+        <img src="images (1).jpg" />
+        <div className="grid grid-cols-12 ">
+            <div className="col span-2">
+                <img className={"rounded-full w-20 h-20"} src="images (1).jpg" />
+
+            </div>
+            <div className="col span-11 pl-5">
+                the jagjit singh the great singer
+
+            </div>
+            <div className="col-span-11 pl-5 text-gray-500 text-base">
+                aashutosh kumar
+            </div>
+            <div className="col-span-11 pl-5">
+                1.2M views . 2 days ago
+
+            </div>
+
+        </div>
+
+      
+    </div>
+}
